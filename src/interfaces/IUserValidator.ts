6d7a0b36af16cd6e1541f627ce83fb5aaf58d04c
Ricardo <ricardo.dev1991@gmail.com>
2024-10-16 10:18:04 -0300

    feat: Adicionando CPF e confirmação de senha na tabela de usuário. Schema modificado e gerado uma nova migration.

--- src/interfaces/IUserValidator.ts
+++ src/interfaces/IUserValidator.ts
@@ -2,6 +2,8 @@ export interface IUserValidator {
     name: string;
     email: string;
     password: string;
+    confirmPassword: string;
+    cpf: string;
     gender: string;
     cityId: number;
 }
