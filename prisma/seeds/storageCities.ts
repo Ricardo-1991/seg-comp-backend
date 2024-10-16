f392711e91171649583b57e21bfd6769e962c414
Ricardo <ricardo.dev1991@gmail.com>
2024-10-16 19:22:16 -0300

    fix: Tipando o seeds de cidade.

--- prisma/seeds/storageCities.ts
+++ prisma/seeds/storageCities.ts
@@ -3,10 +3,34 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+interface City {
+  id: number;
+  nome: string;
+  microrregiao: {
+    id: number;
+    nome: string;
+    mesorregiao: {
+      id: number;
+      nome: string;
+      UF: {
+        id: number;
+        sigla: string;
+        nome: string;
+        regiao: {
+          id: number;
+          sigla: string;
+          nome: string;
+        };
+      };
+    };
+  };
+}
+
+
 async function getCitiesofBahia() {
   try {
     const response = await axios.get('https://servicodados.ibge.gov.br/api/v1/localidades/estados/BA/municipios');
-    return response.data.map((city: any) => city.nome);
+    return response.data.map((city: City) => city.nome);
   } catch (error) {
     console.error('Erro ao obter dados das cidades:', error);
     return [];
